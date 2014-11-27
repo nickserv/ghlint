0afd22cd3e42f35bfbbe1723495c32c8b65f5b88
Nicolas McCurdy <thenickperson@gmail.com>
2014-11-27 13:34:02 -0500

    Show CLI results in a table

--- cli.js
+++ cli.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 var ghlint = require('./index');
+var Table = require('cli-table');
 
 ghlint.lintAll(function (err, linters) {
   if (err) {
     console.error(err);
   } else {
+    var table = new Table({ head: ['Linter', 'Status'] });
     linters.forEach(function (linter) {
-      console.log(linter.message + ': ' + linter.result);
+      table.push([linter.message, linter.result]);
     });
+    console.log(table.toString());
   }
 });
