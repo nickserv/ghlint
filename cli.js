5ebb9eb202c4ec6f3502ceb6236e94a67d6f5dae
Nicolas McCurdy <thenickperson@gmail.com>
2014-12-09 17:24:50 -0500

    Extract a printResults() function

--- cli.js
+++ cli.js
@@ -2,17 +2,21 @@
 var ghlint = require('./index');
 var Table = require('cli-table');
 
+function printResults(results) {
+  var table = new Table({ head: ['Linter', 'Status'] });
+  results.forEach(function (result) {
+    table.push([result.message, result.result]);
+  });
+  console.log(table.toString());
+}
+
 var repo = process.argv[2];
 if (repo) {
   ghlint.lintAll(repo, function (err, linters) {
     if (err) {
       console.error(err);
     } else {
-      var table = new Table({ head: ['Linter', 'Status'] });
-      linters.forEach(function (linter) {
-        table.push([linter.message, linter.result]);
-      });
-      console.log(table.toString());
+      printResults(linters);
     }
   });
 } else {
